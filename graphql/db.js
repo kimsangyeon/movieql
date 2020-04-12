840b23cc91724dcf5e6ffcca02f763ad5fb81535
Kim Sangyeon <dho113@naver.com>
2020-04-12 10:34:19 +0900

    #9 Creating first Mutation

--- graphql/db.js
+++ graphql/db.js
@@ -1,37 +1,54 @@
-export const people = [
+export let movies = [
   {
     id: 1,
-    name: 'sangyeon',
-    age: 10,
-    gender: 'male',
+    name: 'Avengers: Endgame (Movie, 2019)',
+    score: 10,
   },
   {
     id: 2,
-    name: 'dandy',
-    age: 14,
-    gender: 'male',
+    name: 'The God father (1972)',
+    score: 8,
   },
   {
     id: 3,
-    name: 'candy',
-    age: 12,
-    gender: 'female',
+    name: 'Frozen',
+    score: 9,
   },
   {
     id: 4,
-    name: 'tom',
-    age: 9,
-    gender: 'male',
+    name: 'Mission Impossible',
+    score: 7,
   },
   {
     id: 5,
-    name: 'janny',
-    age: 16,
-    gender: 'female',
+    name: 'Suicide Squad',
+    score: 7,
   },
 ];
 
-export const getById = (id) => {
-  const filterPeople = people.filter((person) => person.id === id);
-  return filterPeople[0];
+export const getMovies = () => movies;
+
+export const getMovieById = (id) => {
+  const filterMovie = movies.filter((movie) => movie.id === id);
+  return filterMovie[0];
+};
+
+export const deleteMovie = (id) => {
+  const cleanedMovies = movies.filter((movie) => movie.id !== id);
+  if (movies.length > cleanedMovies.length) {
+    movies = cleanedMovies;
+    return true;
+  } else {
+    return false;
+  }
+};
+
+export const addMovie = (name, score) => {
+  const newMovie = {
+    id: movies.length + 1,
+    name,
+    score,
+  };
+  movies.push(newMovie);
+  return newMovie;
 };
