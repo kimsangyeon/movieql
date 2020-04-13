8bfa79ac90f78bbab8a3ce1bff04bd7e874e8c6f
Kim Sangyeon <dho113@naver.com>
2020-04-13 23:04:57 +0900

    #11 Wrapping a REST API width GraphQL Part One

--- graphql/db.js
+++ graphql/db.js
@@ -1,54 +1,9 @@
-export let movies = [
-  {
-    id: 1,
-    name: 'Avengers: Endgame (Movie, 2019)',
-    score: 10,
-  },
-  {
-    id: 2,
-    name: 'The God father (1972)',
-    score: 8,
-  },
-  {
-    id: 3,
-    name: 'Frozen',
-    score: 9,
-  },
-  {
-    id: 4,
-    name: 'Mission Impossible',
-    score: 7,
-  },
-  {
-    id: 5,
-    name: 'Suicide Squad',
-    score: 7,
-  },
-];
+import fetch from 'node-fetch';
 
-export const getMovies = () => movies;
+const API_URL = 'https://yts.am/api/v2/list_movies.json';
 
-export const getMovieById = (id) => {
-  const filterMovie = movies.filter((movie) => movie.id === id);
-  return filterMovie[0];
-};
-
-export const deleteMovie = (id) => {
-  const cleanedMovies = movies.filter((movie) => movie.id !== id);
-  if (movies.length > cleanedMovies.length) {
-    movies = cleanedMovies;
-    return true;
-  } else {
-    return false;
-  }
-};
-
-export const addMovie = (name, score) => {
-  const newMovie = {
-    id: movies.reduce((id, movie) => Math.max(id, movie.id + 1), 0),
-    name,
-    score,
-  };
-  movies.push(newMovie);
-  return newMovie;
+const getMoveis = (limit, rating) => {
+  fetch(`${API_URL}`)
+    .then((res) => res.json())
+    .then((json) => json.data.movies);
 };
